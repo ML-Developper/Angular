88adbe3a415b8ff2a748c1acafec5c89ae87c2dc
MARWEN <115922126+ML-Developper@users.noreply.github.com>
2025-08-07 17:50:04 +0100

    Add files via upload

---
+++ ng2-chart-example/src/app/table-diagramme/table-diagramme.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-diagramme',
+  standalone: false,
+  templateUrl: './table-diagramme.html',
+  styleUrl: './table-diagramme.css'
+})
+export class TableDiagramme {
+
+fournisseur = "Fournisseur";
+revenue = 55 ;
+contributionCA = 55;
+profit = 55;
+contributionProfit = 55;
+nombresCommandes = 55;
+stockActif = 55;
+
+}
